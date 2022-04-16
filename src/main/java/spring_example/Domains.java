000e3c301a0f1fffa973cb75c5403de41f966f86
Aidyl <jgarciaalbalah@gmail.com>
2022-04-15 22:11:05 -0400

    Domains: New Domains the ones to traslate

---
+++ src/main/java/spring_example/Domains.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spring_example;
+
+/**
+ *
+ * @author Aidyl
+ */
+public class Domains {
+
+    public record NewArea(String name, String desc) {
+
+    }
+}
