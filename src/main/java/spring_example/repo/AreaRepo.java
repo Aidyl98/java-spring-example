61d9252c044ef03b24f6eee6aa4eece057ef12b0
Aidyl <jgarciaalbalah@gmail.com>
2022-04-15 22:40:00 -0400

    AreaRepo: + Repo folder

--- src/main/java/spring_example/repo/AreaRepo.java
+++ src/main/java/spring_example/repo/AreaRepo.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package spring_example;
+package spring_example.repo;
 
+import spring_example.entitys.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
